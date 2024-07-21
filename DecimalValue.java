fe38a6a0bd48abb158a830d25cf6fee177b4953d
piyush_lasane <160647609+piyushlasane@users.noreply.github.com>
2024-07-21 22:39:47 +0530

    Create DecimalValue.java

---
+++ DecimalValue.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) { this.val = val; }
+}
+
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int dec = 0;
+        StringBuilder s = new StringBuilder();
+        while (head != null) {
+            s.append(head.val);
+            head = head.next;
+        }
+        s.reverse();
+        for (int i = 0, k = 0; i < s.length(); i++, k++) {
+            if (s.charAt(i) == '1') {
+                dec += Math.pow(2, k);
+            }
+        }
+        return dec;
+    }
+}
+
+public class DecimalValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the linked list: ");
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            System.out.println("The linked list is empty.");
+            return;
+        }
+
+        System.out.println("Enter the elements (0 or 1 only): ");
+        ListNode head = new ListNode(sc.nextInt());
+        ListNode current = head;
+
+        for (int i = 1; i < n; i++) {
+            current.next = new ListNode(sc.nextInt());
+            current = current.next;
+        }
+
+        Solution solution = new Solution();
+        int decimalValue = solution.getDecimalValue(head);
+
+        System.out.println("The decimal value is: " + decimalValue);
+        sc.close();
+    }
+}
