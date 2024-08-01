06597f0f6a3b5290578e4d132e942d8a368eaef1
piyush_lasane <160647609+piyushlasane@users.noreply.github.com>
2024-08-01 20:50:37 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 class Solution {
     public int reverse(int x) {
-        int ans = 0;
+        int res = 0;
         for (; x != 0; x /= 10) {
-            if (ans < Integer.MIN_VALUE / 10 || ans > Integer.MAX_VALUE / 10) {
+            if (res < Integer.MIN_VALUE / 10 || res > Integer.MAX_VALUE / 10) {
                 return 0;
             }
-            ans = ans * 10 + x % 10;
+            res = res * 10 + x % 10;
         }
-        return ans;
+        return res;
     }
 }
 
