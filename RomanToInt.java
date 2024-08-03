597322db44a467aa1d2274afabd0ea59bc07a203
piyush_lasane <160647609+piyushlasane@users.noreply.github.com>
2024-08-03 14:38:02 +0530

    Update RomanToInt.java

--- RomanToInt.java
+++ RomanToInt.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 class Solution {
     public int romanToInt(String s) {
-        String cs = "IVXLCDM";
-        int[] vs = {1, 5, 10, 50, 100, 500, 1000};
-        Map<Character, Integer> d = new HashMap<>();
-        for (int i = 0; i < vs.length; ++i) {
-            d.put(cs.charAt(i), vs[i]);
+        String RI = "IVXLCDM";
+        int[] DI = {1, 5, 10, 50, 100, 500, 1000};
+        Map<Character, Integer> m = new HashMap<>();
+        for (int i = 0; i < DI.length; ++i) {
+            m.put(RI.charAt(i), DI[i]);
         }
         int n = s.length();
-        int ans = d.get(s.charAt(n - 1));
+        int ans = m.get(s.charAt(n - 1));
         for (int i = 0; i < n - 1; ++i) {
-            int sign = d.get(s.charAt(i)) < d.get(s.charAt(i + 1)) ? -1 : 1;
-            ans += sign * d.get(s.charAt(i));
+            int sign = d.get(s.charAt(i)) < m.get(s.charAt(i + 1)) ? -1 : 1;
+            ans += sign * m.get(s.charAt(i));
         }
         return ans;
     }
